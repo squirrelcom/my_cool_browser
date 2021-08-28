dfd561d35f660e09986673e9f18917d153d573c8
squirrelcom <84091436+squirrelcom@users.noreply.github.com>
2021-08-28 09:33:56 +0100

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
         self.browser = QWebEngineView()
-        self.browser.setUrl(QUrl('http://google.com'))
+        self.browser.setUrl(QUrl('index.html'))
         self.setCentralWidget(self.browser)
         self.showMaximized()
 
@@ -20,22 +20,8 @@ class MainWindow(QMainWindow):
         back_btn.triggered.connect(self.browser.back)
         navbar.addAction(back_btn)
 
-        forward_btn = QAction('Forward', self)
-        forward_btn.triggered.connect(self.browser.forward)
-        navbar.addAction(forward_btn)
-
-    def navigate_home(self):
-        self.browser.setUrl(QUrl('http://programming-hero.com'))
-
-    def navigate_to_url(self):
-        url = self.url_bar.text()
-        self.browser.setUrl(QUrl(url))
-
-    def update_url(self, q):
-        self.url_bar.setText(q.toString())
-
 
 app = QApplication(sys.argv)
-QApplication.setApplicationName('My Cool Browser')
+QApplication.setApplicationName('ChipOS')
 window = MainWindow()
 app.exec_()
